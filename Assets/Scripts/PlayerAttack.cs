e0bd7dc525364368be81a62385abf816261ad03b
ypyec <ypy23x@gmail.com>
2016-04-28 11:07:00 -0500

    inicial con las configuraciones del andy que siguen siendo 1GB

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{         
+	Vector3 movement;                   
+	Animator anim;                      
+	Rigidbody playerRigidbody;          
+
+	void Awake ()
+	{
+		movement = new Vector3 (0f, 0f, 0f);
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		Animating ();
+	}
+
+
+	void Animating ()
+	{
+		if (Input.GetAxisRaw ("Fire1") != 0) {
+			anim.SetTrigger ("Hit");
+		} else if (Input.GetAxisRaw ("Fire2") != 0) {
+			anim.SetTrigger ("Hit2");
+		} else {
+			anim.ResetTrigger ("Hit");
+			anim.ResetTrigger ("Hit2");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;            
+
+	Vector3 movement;                   
+	Animator anim;                      
+	Rigidbody playerRigidbody;          
+
+	void Awake ()
+	{
+		movement = new Vector3 (0f, 0f, 0f);
+		anim = GetComponent <Animator> ();
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Animating (h, v);
+	}
+
+	void Move (float h, float v)
+	{
+		if(movement.x != h || movement.z != v)
+			transform.forward = movement;
+
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+		
+
+	void Animating (float h, float v)
+	{
+		bool walking = false;
+		if (v != 0f || h != 0f)
+			walking = true;
+
+		anim.SetBool ("Walking", walking);	
+	}
+}
