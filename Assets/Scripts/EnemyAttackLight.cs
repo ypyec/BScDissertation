a626cd3c9ffabcf607c61d1b10d5ab15c0f40aac
Andres Ruiz <andresruiz@Andress-MacBook-Pro.local>
2016-05-02 22:16:30 -0500

    Agregados los enemigos a la escena test ademas agregado el navmesh

---
+++ Assets/Scripts/EnemyAttackLight.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttackLight : MonoBehaviour
+{                 
+	public GameObject wave;
+
+	public GameObject basicShot;
+	public GameObject basicShotOrigin;
+	public int basicShotDMG = 5;                  
+	public float basicShotCD = 0.65f;
+
+	Animator anim;
+	float cooldown;  
+	bool attacking;
+
+	void Awake ()
+	{
+		anim = GetComponent <Animator> ();
+		cooldown = basicShotCD;
+	}
+
+
+	void FixedUpdate ()
+	{
+		cooldown += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && cooldown >= basicShotCD)
+		{
+			StartCoroutine (animateAttack ());
+		} else {
+			anim.ResetTrigger ("Hit");
+		}
+	}
+
+	IEnumerator animateAttack(){
+		cooldown = 0f;
+		anim.SetTrigger ("Hit");
+
+		yield return new WaitForSeconds (0.27f);
+
+		attack ();
+
+	}
+
+	void attack(){
+
+		GameObject s1 = (GameObject)Instantiate(basicShot, basicShotOrigin.transform.position, this.transform.rotation);
+		s1.GetComponent<BeamParam>().SetBeamParam(this.GetComponent<BeamParam>());
+
+		GameObject wav = (GameObject)Instantiate(wave, this.transform.position, this.transform.rotation);
+		wav.transform.localScale *= 0.25f;
+		wav.transform.Rotate(Vector3.left, 90.0f);
+		wav.GetComponent<BeamWave>().col = this.GetComponent<BeamParam>().BeamColor;
+
+
+	}
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+	Transform player;               // Reference to the player's position.
+	//PlayerHealth playerHealth;      // Reference to the player's health.
+	//EnemyHealth enemyHealth;        // Reference to this enemy's health.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	SphereCollider range;
+	Animator anim;
+	bool walk;
+
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		//playerHealth = player.GetComponent <PlayerHealth> ();
+		//enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent> ();
+		range = GetComponent <SphereCollider> ();
+		anim = GetComponent <Animator> ();
+	}
+
+
+	void Update ()
+	{
+		// If the enemy and the player have health left...
+		//if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		//{
+			// ... set the destination of the nav mesh agent to the player.
+			
+		//}
+		// Otherwise...
+		//else
+		//{
+			// ... disable the nav mesh agent.
+		//	nav.enabled = false;
+		//}
+	}
+
+	void OnTriggerEnter(Collider enemy) {
+		if (enemy.CompareTag ("Player")) {
+			walk = true;
+			Animating (walk);
+			nav.SetDestination (player.position);
+		}
+	}
+
+	void OnTriggerStay(Collider enemy) {
+		if (enemy.CompareTag ("Player")) {
+			walk = true;
+			Animating (walk);
+			nav.SetDestination (player.position);
+		}
+	}
+
+	void OnTriggerExit(Collider player) {
+		if (player.CompareTag ("Player")) {
+			walk = false;
+			Animating (walk);
+		}
+	}
+
+	void Animating (bool walking)
+	{
+		anim.SetBool ("isWalking", walking);	
+	}
+}
