3d37b00c1c871a7eeeb27c6424feb3b1738119d6
ypyec <ypy23x@gmail.com>
2016-05-14 15:50:23 -0500

    agregado un highscore

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -2,31 +2,21 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ScoreManager : MonoBehaviour
+public class HighScoreManager : MonoBehaviour
 {
-	public static int score;        // The player's score.
-
-
 	Text text;                      // Reference to the Text component.
-	int highscore;
 
 	void Awake ()
 	{
 		// Set up the reference.
 		text = GetComponent <Text> ();
-		highscore = PlayerPrefs.GetInt("High Score");
-		// Reset the score.
-		score = 0;
 	}
 
 
 	void Update ()
 	{
 		// Set the displayed text to be the word "Score" followed by the score value.
-		if (score > highscore) {
-			PlayerPrefs.SetInt ("High Score", score);
-		}
-		text.text = "Puntaje actual: " + score;
+		text.text = "Mejor Puntaje: " + PlayerPrefs.GetInt("High Score");;
 
 	}
 }
