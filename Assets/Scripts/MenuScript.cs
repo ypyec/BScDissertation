ce7b4253f8fc0eed051da0c961a0606cfa4066cd
Andres Ruiz <andresruiz@Andress-MacBook-Pro.local>
2016-05-31 02:13:48 -0500

    Diseñado el nivel sin enemigos solo aspecto gráfico

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,27 +6,35 @@ using System.Collections;
 public class MenuScript : MonoBehaviour {
 
 	public Canvas quitMenu;
-	public Button startText;
+	public Button campaignText;
+	public Button arcadeText;
 	public Button exitText;
 
 	// Use this for initialization
 	void Start () {
 		quitMenu = quitMenu.GetComponent<Canvas> ();
-		startText = startText.GetComponent<Button> ();
+		campaignText = campaignText.GetComponent<Button> ();
+		arcadeText = arcadeText.GetComponent<Button> ();
 		exitText = exitText.GetComponent<Button> ();
 		quitMenu.enabled = false;
 	}
 
 	public void ExitPress() {
 		quitMenu.enabled = true;
-		startText.enabled = false;
+		campaignText.enabled = false;
+		arcadeText.enabled = false;
 		exitText.enabled = false;
 	}
 
 	public void NoPress() {
 		quitMenu.enabled = false;
-		startText.enabled = true;
+		campaignText.enabled = true;
+		arcadeText.enabled = true;
 		exitText.enabled = true;
+	}
+
+	public void StartCampaign() {
+		SceneManager.LoadScene ("Level-0");
 	}
 
 	public void StartArcade() {
