714454a4e0fa2bbdc8dea4e0a82a52fb6ba8569b
ypyec <ypy23x@gmail.com>
2016-05-30 23:24:05 -0500

    creado spawner. BUG: crashea al momento de entrar en contacto un enemigo con el player

---
+++ Assets/Scripts/ProximitySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximitySpawner : MonoBehaviour {
+
+	public GameObject enemy;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.GetComponent <PlayerHealth> ()) {
+			//Invoke("spawnEnemy", 0.5f); 
+			spawnEnemy ();
+
+		} 
+	}
+
+	void spawnEnemy(){
+		gameObject.SetActive(false);
+		Instantiate (enemy, transform.position, transform.rotation);
+
+	}
+}
