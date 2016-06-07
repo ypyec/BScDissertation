df549b2aeec81e45785c8f64cdce82a2cae833b8
Andres Ruiz <andresruiz@Andress-MacBook-Pro.local>
2016-06-07 01:44:09 -0500

    Agregado control del menu pause a base de teclas

--- Assets/Scripts/Game Manager/PauseScript.cs
+++ Assets/Scripts/Game Manager/PauseScript.cs
@@ -10,12 +10,15 @@ public class PauseScript : MonoBehaviour {
 	public Button exitText;
 	public static bool paused;
 
-
+	private int index;
+	private float timer;
 	Canvas pauseMenu;
 
 
 	// Use this for initialization
 	void Start () {
+		index = 0;
+		timer = 0.5f;
 		pauseMenu = this.GetComponent<Canvas> ();
 		continueText = continueText.GetComponent<Button> ();
 		exitText = exitText.GetComponent<Button> ();
@@ -24,20 +27,44 @@ public class PauseScript : MonoBehaviour {
 	}
 
 	void Update() {
+		timer += Time.unscaledDeltaTime;
 		if (Input.GetButton ("Cancel")) {
 			if (!paused) {
 				pauseMenu.enabled = paused = true;
 				Time.timeScale = 0;
+				PauseSelect (index);
 			}
+		}
+		if (pauseMenu.enabled) {
+			if (Input.GetAxisRaw ("Vertical") == -1 && timer >= 0.3f) {
+				if (index == 1)
+					index = 0;
+				else
+					index++;
+				PauseSelect (index);
+			} else if (Input.GetAxisRaw ("Vertical") == 1 && timer >= 0.3f) {
+				if (index == 0)
+					index = 1;
+				else
+					index--;
+				PauseSelect (index);
+			} 
 		}
 	}
 
-
-	public void NoPress() {
-		exitText.enabled = true;
-		continueText.enabled = true;
+	private void PauseSelect(int MenuIndex) {
+		print (index);
+		switch (MenuIndex) {
+		case 0:
+			continueText.Select ();
+			timer = 0f;
+			break;
+		case 1:
+			exitText.Select ();
+			timer = 0f;
+			break;
+		}
 	}
-
 
 	public void Continue() {
 		pauseMenu.enabled = paused = false;
